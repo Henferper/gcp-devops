8b9fe210efe510fca29e51a456c7611fabb63764
Henrique Fernandes <henriquealcalde@hotmail.com>
2025-01-28 13:02:46 -0300

    :sparkles:feat:acess the uniform-bucket

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
-resource "google_storage_bucket" "auto-expire"{
+resource "google_storage_bucket" "auto-expire" {
   name          = "tt-cicdbuild-bucket-010203"
   location      = "southamerica-east1"
   project       = "integracaohomologado"
   force_destroy = true
 
   public_access_prevention = "enforced"
+
+  # Habilitando Uniform Bucket
+  uniform_bucket_level_access = true
 }
