1d32f32e7b334ed63fceeb3e42b419318a1894f4
Henrique Fernandes <henriquealcalde@hotmail.com>
2025-01-28 15:34:58 -0300

    :sparkles:feat:comment the main.ft

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-resource "google_storage_bucket" "auto-expire" {
-  name          = "tt-cicdbuild-bucket-unique-20250124"
-  location      = "southamerica-east1"
-  project       = "integracaohomologado"
-  force_destroy = true
+# resource "google_storage_bucket" "auto-expire" {
+#  name          = "tt-cicdbuild-bucket-unique-20250124"
+#  location      = "southamerica-east1"
+#  project       = "integracaohomologado"
+#  force_destroy = true
 
-  public_access_prevention = "enforced"
+#  public_access_prevention = "enforced"
 
   # Habilitando Uniform Bucket
-  uniform_bucket_level_access = true
-}
+#  uniform_bucket_level_access = true
+#}
